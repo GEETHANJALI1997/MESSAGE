80a9c3743ec58502bb76fe86fce46a2108139a1a
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-06-04 21:47:11 +0530

    Create sreverse.java

---
+++ sreverse.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class sreverse
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+string s1=s.reverse();
+System.out.print(s1);
+}
+}
