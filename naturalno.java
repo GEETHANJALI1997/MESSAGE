7bbdde5101d3c1b265cad28fb9344f317039d24a
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-06-05 14:46:04 +0530

    Create naturalno.java

---
+++ naturalno.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class naturalno
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a=0;
+if(n>a)
+{
+int x,y
+x=n*(n+1);
+y=x/2;
+System.out.print(y);
+}
+}
+}
