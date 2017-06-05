3da1154a133d1d61f42bda03d80f042435fedc5c
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-06-05 17:29:47 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+class NNO
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int s=0;
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(a[i]==a[j])
+{ s=1;
+System.out.println(a[i]);
+}
+}
+}
+if(s==0)
+{
+System.out.println("no repeatable elements");
+}
+}
+}
