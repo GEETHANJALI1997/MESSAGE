127fd8297e4ef7cdde172e17d09eb9cbe49a0b19
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-06-22 08:46:31 -0700

    Update wordreverse.java

--- wordreverse.java
+++ wordreverse.java
@@ -1,1 +1,19 @@
 
+import java.io.*;
+import java.util.*;
+class wordreverse
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        String a[]=input.split(" ");
+        for(int i=0;i<a.length;i++)
+        {
+            StringBuffer sb=new StringBuffer(a[i]);
+            String res=sb.reverse().toString();
+            System.out.print(res+" ");
+        }
+    }
+}
+
