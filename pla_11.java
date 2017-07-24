81a939fb5bc80946384820daf550a2b3f224f570
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-07-24 15:20:14 +0530

    Create pla_11.java

---
+++ pla_11.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class pla_11
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next().toLowerCase();
+        String a[]={"sunday","monday","tuesday","wdenesday","thursday","friday","saturday"};
+        if(s==a[0])
+        System.out.print("false");
+        else
+        System.out.print("true");
+        
+    }
+}
+
