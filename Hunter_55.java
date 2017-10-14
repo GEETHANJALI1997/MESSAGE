9e7e493e6cd777322bffc8bf6cf0eae7c66ec303
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-10-14 10:34:51 +0530

    Create Hunter_55.java

---
+++ Hunter_55.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Hunter_55
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         int noetorotate=sc.nextInt();
+         int aArrayList<Integer> al=new ArrayList<Integer>();
+         for(int i=noetorotate;i<n;i++)
+         al.add(arr[i]);
+         rr[]=new int[n];
+              arr[i]=sc.nextInt();
+             for(int i=0;i<n;i++)
+for(int i=0;i<n-noetorotate-1;i++)
+         al.add(arr[i]);
+         for(int i=0;i<al.size();i++)
+         System.out.print(i!=al.size()-1?al.get(i)+" ":al.get(i));
+    }
+}
